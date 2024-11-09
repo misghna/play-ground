bb3940c7dfb8805738a224b9047d593f391a89e1
misghna <sessnu@gmail.com>
2024-11-09 13:06:26 -0600

    Update image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -3,78 +3,39 @@ import boto3
 import schedule
 import time
 from datetime import datetime
+from util import camera_ip
+from aws_client import get_aws_session
 from botocore.exceptions import NoCredentialsError
 from util import get_device_id
-from aws_client import get_aws_session
+from util import write_log
+import requests
 import socket
 
+# AWS S3 credentials and bucket information
 bucket_name = 's-camera'
 device_id = get_device_id()
 # Axis camera URL
-axis_camera_ip = '192.168.1.223'
-axis_camera_url = 'http://{axis_camera_ip}/mjpg/video.mjpg'
+axis_camera_url = f"http://{camera_ip()}/jpg/image.jpg"
 
 
-def write_log(text, session_token=None):
-    timestamp_millis = int(time.time() * 1000)
-    # Define the S3 object key (path)
-    object_key = f'logs/{device_id}_{timestamp_millis}.log'
+def capture_image_from_axis() -> bytes:
 
-    # Create an S3 client
-    s3_client = get_aws_session()
-
-    try:
-        # Upload the text string as an object to S3
-        msg = f"{timestamp_millis},{device_id},{text}"
-        s3_client.put_object(
-            Bucket=bucket_name,
-            Key=object_key,
-            Body=msg,
-            ContentType='text/plain'
-        )
-        print(f"Text uploaded successfully to {bucket_name}/{object_key}.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-def check_tcp_connection(ip, port, timeout=5):
-    # Check if a TCP connection to the specified IP and port can be established.
-    try:
-        with socket.create_connection((ip, port), timeout):
-            return True
-    except (socket.timeout, socket.error):
-        write_log("Error: connecting to camera")
-        print("Error: connecting to camera")
-        return False
+    auth_header = 'Digest username="root", realm="AXIS_B8A44FBE3641", nonce="rfzFw5AlBgA=67b5b06cbb1f3a58df46db7d9da7381973a4432f", uri="/axis-cgi/param.cgi", algorithm=MD5, response="f1e8f807813bc2f0f303ff5e3d77bf97", qop=auth, nc=0000004d, cnonce="d94b70e736de23a3"'
+    headers = {
+        'Authorization': auth_header
+    }
     
-def capture_image_from_axis():
-    # Initialize video capture from the Axis camera
-    if(not check_tcp_connection(axis_camera_ip,80)):
-       return None
-    # Initialize video capture from the Axis camera
-    cap = cv2.VideoCapture(axis_camera_url)
-    if not cap.isOpened():
-        write_log("Error: Unable to open camera image")
-        print("Error: Unable to open video stream.")
-        return None
-
-    ret, frame = cap.read()
-    cap.release()
-
-    if not ret:
-        write_log("Error: Unable to capture image")
-        print("Error: Unable to capture image.")
-        return None
-
-    # Encode the frame as JPEG
-    success, encoded_image = cv2.imencode('.jpg', frame)
-    if not success:
-        write_log("Error: Failed to encode image")
-        print("Error: Failed to encode image.")
-        return None
-
-    return encoded_image.tobytes()
+    # Send the GET request with authorization headers
+    print("capturing image ..")
+    response = requests.get(axis_camera_url, headers=headers)
     
+    # Check if the request was successful
+    if response.status_code == 200:
+        print(f"Received image of {len(response.content)} bytes.")
+        return response.content  # Return the byte array of the image
+    else:
+        print("error getting image ..")
+        response.raise_for_status()  # Raise an error if the request failed
 
 def upload_image_to_s3(image_bytes):
 
@@ -113,13 +74,16 @@ def capture_and_upload():
         print(f"An error occurred in capture_and_upload: {e}")
 
 # Schedule the task to run every 15 minutes at 00, 15, 30, and 45 minutes past the hour
-schedule.every().hour.at(":00").do(capture_and_upload)
+schedule.every().hour.at(":04").do(capture_and_upload)
 schedule.every().hour.at(":15").do(capture_and_upload)
 schedule.every().hour.at(":30").do(capture_and_upload)
 schedule.every().hour.at(":45").do(capture_and_upload)
 
 def run_scheduler():
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        try:
+            schedule.run_pending()
+            time.sleep(1)
+        except Exception as e:
+            print("error running scheduler")
 
