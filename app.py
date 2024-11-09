85d65bf0f540dded0fc5ceccbedc4024b1352932
misghna <sessnu@gmail.com>
2024-11-09 13:05:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import threading
 from image_processor import run_scheduler
 from image_processor import write_log
+from websocket_listener import run_ws
 import time
 
 # Run the scheduler in a separate thread
 scheduler_thread = threading.Thread(target=run_scheduler)
 scheduler_thread.daemon = True
 scheduler_thread.start()
+
+
+# Start the WebSocket connection in a separate thread
+thread = threading.Thread(target=run_ws, daemon=True)
+thread.start()
+
 
 # Main thread can perform other tasks
 try:
